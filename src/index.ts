10f7c4f416b1243258e9c87282d4f25eb63e68c8
Yani-Haag <104107528+Yani-Haag@users.noreply.github.com>
2022-05-15 12:35:11 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+console.log("Pasos para realizar una pizza");
+
+console.log("Precalentar el horno a 180°");
+console.log("Mezclar levadura, agua, azucar y dejar reposar por 15 minutos");
+console.log("Poner harina, sal y mezclar");
+console.log("Agregarle a los secos el fermento de la levadura");
+console.log("Agragarle unas cucharadas de aceite");
+console.log("Integrar todo hasta que quede un bollo liso");
+console.log("Dejar reposar por 30 minutos");
+console.log("Amasar nuevamente");
+console.log("Poner la masa en un a pizzera previamente aceitada y dejar levar nuevamente");
+console.log("Mezclar pimenton con agua y esparcirlo por toda la masa");
+console.log("Cocinar en el horno por 15 minutos a 180°")
+console.log("Por ultimo sacarla del horno");
